dde33a1bc8bbac5749e569bd4540544594e05c80
rahultoday <mail@rahul.today>
2016-08-13 15:12:35 +0530

    Port changed to 3000.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var debug = require('debug')('P1');
 //   });
 
 var app = require('./server');
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
